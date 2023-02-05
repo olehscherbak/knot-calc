b9389520825a70c46509600fe97a55a512387a8c
Oleh.Scherbak <oleh.scherbak@ukr.net>
2023-02-05 14:21:50 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span className="App-header-text">
+          Розрахунок кількості вузлів у сітці.
+        </span>
       </header>
+
+      <form className="form">
+        <label className="label">
+          висота
+          <input type="number" min="1" max="10" className="input"></input>
+          <span className="units">м</span>
+        </label>
+        <label className="label">
+          ширина
+          <input type="text" className="input"></input>
+          <span className="units">м</span>
+        </label>
+        <label className="label">
+          гратка
+          <input type="text" className="input"></input>
+          <span className="units">cм</span>
+        </label>
+      </form>
     </div>
   );
 }
